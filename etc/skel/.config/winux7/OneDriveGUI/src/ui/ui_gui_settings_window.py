b7a0ceb0484d67ffc29187cc735d86389f0b72c6
MacroHard-Winux <129944538+MacroHard-Winux@users.noreply.github.com>
2024-01-27 22:48:11 +1100

    Folder failed to upload :/

---
+++ etc/skel/.config/winux7/OneDriveGUI/src/ui/ui_gui_settings_window.py
@@ -0,0 +1,232 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'gui_settings_window.ui'
+##
+## Created by: Qt User Interface Compiler version 6.2.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QCheckBox, QComboBox, QFormLayout,
+    QGridLayout, QGroupBox, QHBoxLayout, QLabel,
+    QLineEdit, QPushButton, QSizePolicy, QSpacerItem,
+    QSpinBox, QVBoxLayout, QWidget)
+
+class Ui_gui_settings_window(object):
+    def setupUi(self, gui_settings_window):
+        if not gui_settings_window.objectName():
+            gui_settings_window.setObjectName(u"gui_settings_window")
+        gui_settings_window.resize(640, 535)
+        self.verticalLayout_2 = QVBoxLayout(gui_settings_window)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.groupBox = QGroupBox(gui_settings_window)
+        self.groupBox.setObjectName(u"groupBox")
+        self.verticalLayout_3 = QVBoxLayout(self.groupBox)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.formLayout = QFormLayout()
+        self.formLayout.setObjectName(u"formLayout")
+        self.checkBox_start_minimized = QCheckBox(self.groupBox)
+        self.checkBox_start_minimized.setObjectName(u"checkBox_start_minimized")
+
+        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.checkBox_start_minimized)
+
+
+        self.verticalLayout_3.addLayout(self.formLayout)
+
+
+        self.verticalLayout.addWidget(self.groupBox)
+
+
+        self.verticalLayout_2.addLayout(self.verticalLayout)
+
+        self.groupBox_4 = QGroupBox(gui_settings_window)
+        self.groupBox_4.setObjectName(u"groupBox_4")
+        self.horizontalLayout_6 = QHBoxLayout(self.groupBox_4)
+        self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
+        self.label_client_bin_path = QLabel(self.groupBox_4)
+        self.label_client_bin_path.setObjectName(u"label_client_bin_path")
+
+        self.horizontalLayout_6.addWidget(self.label_client_bin_path)
+
+        self.lineEdit_client_bin_path = QLineEdit(self.groupBox_4)
+        self.lineEdit_client_bin_path.setObjectName(u"lineEdit_client_bin_path")
+
+        self.horizontalLayout_6.addWidget(self.lineEdit_client_bin_path)
+
+        self.pushButton_client_bin_path = QPushButton(self.groupBox_4)
+        self.pushButton_client_bin_path.setObjectName(u"pushButton_client_bin_path")
+
+        self.horizontalLayout_6.addWidget(self.pushButton_client_bin_path)
+
+
+        self.verticalLayout_2.addWidget(self.groupBox_4)
+
+        self.groupBox_3 = QGroupBox(gui_settings_window)
+        self.groupBox_3.setObjectName(u"groupBox_3")
+        self.gridLayout = QGridLayout(self.groupBox_3)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.checkBox_frameless_window = QCheckBox(self.groupBox_3)
+        self.checkBox_frameless_window.setObjectName(u"checkBox_frameless_window")
+
+        self.gridLayout.addWidget(self.checkBox_frameless_window, 0, 0, 1, 1)
+
+        self.checkBox_combined_start_stop_button = QCheckBox(self.groupBox_3)
+        self.checkBox_combined_start_stop_button.setObjectName(u"checkBox_combined_start_stop_button")
+
+        self.gridLayout.addWidget(self.checkBox_combined_start_stop_button, 1, 0, 1, 1)
+
+
+        self.verticalLayout_2.addWidget(self.groupBox_3)
+
+        self.groupBox_2 = QGroupBox(gui_settings_window)
+        self.groupBox_2.setObjectName(u"groupBox_2")
+        self.verticalLayout_4 = QVBoxLayout(self.groupBox_2)
+        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.label_debug_level = QLabel(self.groupBox_2)
+        self.label_debug_level.setObjectName(u"label_debug_level")
+
+        self.horizontalLayout_3.addWidget(self.label_debug_level)
+
+        self.comboBox_debug_level = QComboBox(self.groupBox_2)
+        self.comboBox_debug_level.addItem("")
+        self.comboBox_debug_level.addItem("")
+        self.comboBox_debug_level.addItem("")
+        self.comboBox_debug_level.addItem("")
+        self.comboBox_debug_level.addItem("")
+        self.comboBox_debug_level.setObjectName(u"comboBox_debug_level")
+
+        self.horizontalLayout_3.addWidget(self.comboBox_debug_level)
+
+        self.horizontalSpacer_3 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_3.addItem(self.horizontalSpacer_3)
+
+
+        self.verticalLayout_4.addLayout(self.horizontalLayout_3)
+
+        self.checkBox_show_debug = QCheckBox(self.groupBox_2)
+        self.checkBox_show_debug.setObjectName(u"checkBox_show_debug")
+
+        self.verticalLayout_4.addWidget(self.checkBox_show_debug)
+
+        self.horizontalLayout_4 = QHBoxLayout()
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.checkBox_save_debug = QCheckBox(self.groupBox_2)
+        self.checkBox_save_debug.setObjectName(u"checkBox_save_debug")
+
+        self.horizontalLayout_4.addWidget(self.checkBox_save_debug)
+
+
+        self.verticalLayout_4.addLayout(self.horizontalLayout_4)
+
+        self.horizontalLayout_5 = QHBoxLayout()
+        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
+        self.label_log_file = QLabel(self.groupBox_2)
+        self.label_log_file.setObjectName(u"label_log_file")
+
+        self.horizontalLayout_5.addWidget(self.label_log_file)
+
+        self.lineEdit_log_file = QLineEdit(self.groupBox_2)
+        self.lineEdit_log_file.setObjectName(u"lineEdit_log_file")
+
+        self.horizontalLayout_5.addWidget(self.lineEdit_log_file)
+
+        self.pushButton_log_file = QPushButton(self.groupBox_2)
+        self.pushButton_log_file.setObjectName(u"pushButton_log_file")
+
+        self.horizontalLayout_5.addWidget(self.pushButton_log_file)
+
+
+        self.verticalLayout_4.addLayout(self.horizontalLayout_5)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.label_log_rotation_interval = QLabel(self.groupBox_2)
+        self.label_log_rotation_interval.setObjectName(u"label_log_rotation_interval")
+
+        self.horizontalLayout.addWidget(self.label_log_rotation_interval)
+
+        self.spinBox_log_rotation_interval = QSpinBox(self.groupBox_2)
+        self.spinBox_log_rotation_interval.setObjectName(u"spinBox_log_rotation_interval")
+
+        self.horizontalLayout.addWidget(self.spinBox_log_rotation_interval)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer)
+
+
+        self.verticalLayout_4.addLayout(self.horizontalLayout)
+
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.label_log_backup_count = QLabel(self.groupBox_2)
+        self.label_log_backup_count.setObjectName(u"label_log_backup_count")
+
+        self.horizontalLayout_2.addWidget(self.label_log_backup_count)
+
+        self.spinBox_log_backup_count = QSpinBox(self.groupBox_2)
+        self.spinBox_log_backup_count.setObjectName(u"spinBox_log_backup_count")
+
+        self.horizontalLayout_2.addWidget(self.spinBox_log_backup_count)
+
+        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_2.addItem(self.horizontalSpacer_2)
+
+
+        self.verticalLayout_4.addLayout(self.horizontalLayout_2)
+
+
+        self.verticalLayout_2.addWidget(self.groupBox_2)
+
+        self.pushButton_save = QPushButton(gui_settings_window)
+        self.pushButton_save.setObjectName(u"pushButton_save")
+
+        self.verticalLayout_2.addWidget(self.pushButton_save)
+
+
+        self.retranslateUi(gui_settings_window)
+
+        QMetaObject.connectSlotsByName(gui_settings_window)
+    # setupUi
+
+    def retranslateUi(self, gui_settings_window):
+        gui_settings_window.setWindowTitle(QCoreApplication.translate("gui_settings_window", u"OneDriveGUI - Settings", None))
+        self.groupBox.setTitle(QCoreApplication.translate("gui_settings_window", u"OneDriveGUI behaviour", None))
+        self.checkBox_start_minimized.setText(QCoreApplication.translate("gui_settings_window", u"Start OneDriveGUI minimized", None))
+        self.groupBox_4.setTitle(QCoreApplication.translate("gui_settings_window", u"OneDrive client options", None))
+        self.label_client_bin_path.setText(QCoreApplication.translate("gui_settings_window", u"Path to OneDrive client binary", None))
+        self.pushButton_client_bin_path.setText(QCoreApplication.translate("gui_settings_window", u"Browse", None))
+        self.groupBox_3.setTitle(QCoreApplication.translate("gui_settings_window", u"Appearance (experimental)", None))
+        self.checkBox_frameless_window.setText(QCoreApplication.translate("gui_settings_window", u"Frameless Window for X11", None))
+        self.checkBox_combined_start_stop_button.setText(QCoreApplication.translate("gui_settings_window", u"Combined Start/Stop button", None))
+        self.groupBox_2.setTitle(QCoreApplication.translate("gui_settings_window", u"Logging", None))
+        self.label_debug_level.setText(QCoreApplication.translate("gui_settings_window", u"Debug level:               ", None))
+        self.comboBox_debug_level.setItemText(0, QCoreApplication.translate("gui_settings_window", u"DEBUG", None))
+        self.comboBox_debug_level.setItemText(1, QCoreApplication.translate("gui_settings_window", u"INFO", None))
+        self.comboBox_debug_level.setItemText(2, QCoreApplication.translate("gui_settings_window", u"WARNING", None))
+        self.comboBox_debug_level.setItemText(3, QCoreApplication.translate("gui_settings_window", u"ERROR", None))
+        self.comboBox_debug_level.setItemText(4, QCoreApplication.translate("gui_settings_window", u"CRITICAL", None))
+
+        self.checkBox_show_debug.setText(QCoreApplication.translate("gui_settings_window", u"Show debug logs in console (stdout)", None))
+        self.checkBox_save_debug.setText(QCoreApplication.translate("gui_settings_window", u"Save debug logs to file", None))
+        self.label_log_file.setText(QCoreApplication.translate("gui_settings_window", u"Log file: ", None))
+        self.pushButton_log_file.setText(QCoreApplication.translate("gui_settings_window", u"Browse", None))
+        self.label_log_rotation_interval.setText(QCoreApplication.translate("gui_settings_window", u"Log rotation (hours): ", None))
+        self.label_log_backup_count.setText(QCoreApplication.translate("gui_settings_window", u"Log backup count:     ", None))
+        self.pushButton_save.setText(QCoreApplication.translate("gui_settings_window", u"Save", None))
+    # retranslateUi
+
---
+++ etc/skel/.config/winux7/OneDriveGUI/src/ui/ui_process_status_page.py
@@ -0,0 +1,259 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'process_status_page.ui'
+##
+## Created by: Qt User Interface Compiler version 6.2.3
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QAbstractItemView, QApplication, QFrame, QHBoxLayout,
+    QLabel, QListWidget, QListWidgetItem, QPushButton,
+    QSizePolicy, QSpacerItem, QVBoxLayout, QWidget)
+
+class Ui_status_page(object):
+    def setupUi(self, status_page):
+        if not status_page.objectName():
+            status_page.setObjectName(u"status_page")
+        status_page.resize(441, 763)
+        status_page.setStyleSheet(u"border: 0px;")
+        self.verticalLayout = QVBoxLayout(status_page)
+        self.verticalLayout.setSpacing(0)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.frame = QFrame(status_page)
+        self.frame.setObjectName(u"frame")
+        self.frame.setMinimumSize(QSize(0, 100))
+        self.frame.setStyleSheet(u"background-color: rgb(0, 120, 212);\n"
+"border: 0px;")
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_2 = QVBoxLayout(self.frame)
+        self.verticalLayout_2.setSpacing(0)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setSpacing(0)
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.horizontalLayout_3.setContentsMargins(5, 5, 5, -1)
+        self.pushButton_quit = QPushButton(self.frame)
+        self.pushButton_quit.setObjectName(u"pushButton_quit")
+        self.pushButton_quit.setMaximumSize(QSize(40, 16777215))
+        self.pushButton_quit.setStyleSheet(u"QToolTip { color: white; }\n"
+"QPushButton:pressed {background-color: rgba(0, 0, 0, 0.1)}")
+        self.pushButton_quit.setIconSize(QSize(20, 20))
+        self.pushButton_quit.setFlat(True)
+
+        self.horizontalLayout_3.addWidget(self.pushButton_quit)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_3.addItem(self.horizontalSpacer)
+
+        self.pushButton_close = QPushButton(self.frame)
+        self.pushButton_close.setObjectName(u"pushButton_close")
+        self.pushButton_close.setMaximumSize(QSize(40, 16777215))
+        self.pushButton_close.setStyleSheet(u"QToolTip { color: white; }\n"
+"QPushButton:pressed {background-color: rgba(0, 0, 0, 0.1)}")
+        self.pushButton_close.setIconSize(QSize(24, 24))
+        self.pushButton_close.setFlat(True)
+
+        self.horizontalLayout_3.addWidget(self.pushButton_close)
+
+
+        self.verticalLayout_2.addLayout(self.horizontalLayout_3)
+
+        self.label_onedrive_status = QLabel(self.frame)
+        self.label_onedrive_status.setObjectName(u"label_onedrive_status")
+        font = QFont()
+        font.setPointSize(11)
+        font.setBold(True)
+        self.label_onedrive_status.setFont(font)
+        self.label_onedrive_status.setStyleSheet(u"color: rgb(255, 255, 255);")
+        self.label_onedrive_status.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout_2.addWidget(self.label_onedrive_status)
+
+        self.label_account_type = QLabel(self.frame)
+        self.label_account_type.setObjectName(u"label_account_type")
+        self.label_account_type.setStyleSheet(u"color: rgb(255, 255, 255);")
+        self.label_account_type.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout_2.addWidget(self.label_account_type)
+
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setSpacing(0)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.horizontalLayout_2.setContentsMargins(5, -1, 0, 5)
+        self.pushButton_start_stop = QPushButton(self.frame)
+        self.pushButton_start_stop.setObjectName(u"pushButton_start_stop")
+        self.pushButton_start_stop.setMaximumSize(QSize(30, 16777215))
+        self.pushButton_start_stop.setStyleSheet(u"QToolTip { color: white; }\n"
+"QPushButton:pressed {background-color: rgba(0, 0, 0, 0.1)}")
+        self.pushButton_start_stop.setIconSize(QSize(22, 22))
+        self.pushButton_start_stop.setFlat(True)
+
+        self.horizontalLayout_2.addWidget(self.pushButton_start_stop)
+
+        self.pushButton_start = QPushButton(self.frame)
+        self.pushButton_start.setObjectName(u"pushButton_start")
+        self.pushButton_start.setMaximumSize(QSize(30, 16777215))
+        self.pushButton_start.setStyleSheet(u"QToolTip { color: white; }\n"
+"QPushButton:pressed {background-color: rgba(0, 0, 0, 0.1)}")
+        self.pushButton_start.setIconSize(QSize(20, 20))
+        self.pushButton_start.setFlat(True)
+
+        self.horizontalLayout_2.addWidget(self.pushButton_start)
+
+        self.pushButton_stop = QPushButton(self.frame)
+        self.pushButton_stop.setObjectName(u"pushButton_stop")
+        self.pushButton_stop.setMaximumSize(QSize(30, 16777215))
+        self.pushButton_stop.setStyleSheet(u"QToolTip { color: white; }\n"
+"QPushButton:pressed {background-color: rgba(0, 0, 0, 0.1)}")
+        self.pushButton_stop.setIconSize(QSize(20, 20))
+        self.pushButton_stop.setFlat(True)
+
+        self.horizontalLayout_2.addWidget(self.pushButton_stop)
+
+        self.label_version_check = QLabel(self.frame)
+        self.label_version_check.setObjectName(u"label_version_check")
+        self.label_version_check.setMaximumSize(QSize(20, 16777215))
+        self.label_version_check.setStyleSheet(u"QToolTip { color: white; }")
+
+        self.horizontalLayout_2.addWidget(self.label_version_check)
+
+        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_2.addItem(self.horizontalSpacer_2)
+
+        self.label_free_space = QLabel(self.frame)
+        self.label_free_space.setObjectName(u"label_free_space")
+        font1 = QFont()
+        font1.setBold(True)
+        self.label_free_space.setFont(font1)
+        self.label_free_space.setStyleSheet(u"color: rgb(255, 255, 255);")
+        self.label_free_space.setAlignment(Qt.AlignRight|Qt.AlignTrailing|Qt.AlignVCenter)
+
+        self.horizontalLayout_2.addWidget(self.label_free_space)
+
+        self.label_free_space_icon = QLabel(self.frame)
+        self.label_free_space_icon.setObjectName(u"label_free_space_icon")
+        self.label_free_space_icon.setMaximumSize(QSize(26, 16777215))
+        self.label_free_space_icon.setStyleSheet(u"QToolTip { color: white; }")
+
+        self.horizontalLayout_2.addWidget(self.label_free_space_icon)
+
+        self.label_status = QLabel(self.frame)
+        self.label_status.setObjectName(u"label_status")
+        self.label_status.setMinimumSize(QSize(30, 0))
+        self.label_status.setMaximumSize(QSize(30, 16777215))
+        self.label_status.setStyleSheet(u"QToolTip { color: white; }")
+
+        self.horizontalLayout_2.addWidget(self.label_status)
+
+
+        self.verticalLayout_2.addLayout(self.horizontalLayout_2)
+
+
+        self.verticalLayout.addWidget(self.frame)
+
+        self.listWidget = QListWidget(status_page)
+        self.listWidget.setObjectName(u"listWidget")
+        self.listWidget.setSelectionMode(QAbstractItemView.NoSelection)
+        self.listWidget.setSortingEnabled(False)
+
+        self.verticalLayout.addWidget(self.listWidget)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setSpacing(0)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(-1, 0, -1, 0)
+        self.pushButton_open_dir = QPushButton(status_page)
+        self.pushButton_open_dir.setObjectName(u"pushButton_open_dir")
+        sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.pushButton_open_dir.sizePolicy().hasHeightForWidth())
+        self.pushButton_open_dir.setSizePolicy(sizePolicy)
+        self.pushButton_open_dir.setStyleSheet(u"QPushButton:pressed { background-color: rgba(0, 0, 0, 10)}")
+        self.pushButton_open_dir.setIconSize(QSize(26, 26))
+        self.pushButton_open_dir.setFlat(True)
+
+        self.horizontalLayout.addWidget(self.pushButton_open_dir)
+
+        self.pushButton_profiles = QPushButton(status_page)
+        self.pushButton_profiles.setObjectName(u"pushButton_profiles")
+        sizePolicy.setHeightForWidth(self.pushButton_profiles.sizePolicy().hasHeightForWidth())
+        self.pushButton_profiles.setSizePolicy(sizePolicy)
+        self.pushButton_profiles.setStyleSheet(u"QPushButton:pressed { background-color: rgba(0, 0, 0, 10)}")
+        self.pushButton_profiles.setIconSize(QSize(26, 26))
+        self.pushButton_profiles.setFlat(True)
+
+        self.horizontalLayout.addWidget(self.pushButton_profiles)
+
+        self.pushButton_gui_settings = QPushButton(status_page)
+        self.pushButton_gui_settings.setObjectName(u"pushButton_gui_settings")
+        sizePolicy.setHeightForWidth(self.pushButton_gui_settings.sizePolicy().hasHeightForWidth())
+        self.pushButton_gui_settings.setSizePolicy(sizePolicy)
+        self.pushButton_gui_settings.setStyleSheet(u"QPushButton:pressed { background-color: rgba(0, 0, 0, 10)}")
+        self.pushButton_gui_settings.setIconSize(QSize(26, 26))
+        self.pushButton_gui_settings.setFlat(True)
+
+        self.horizontalLayout.addWidget(self.pushButton_gui_settings)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+
+        self.retranslateUi(status_page)
+
+        QMetaObject.connectSlotsByName(status_page)
+    # setupUi
+
+    def retranslateUi(self, status_page):
+        status_page.setWindowTitle(QCoreApplication.translate("status_page", u"OneDriveGUI - Process Status", None))
+#if QT_CONFIG(tooltip)
+        self.pushButton_quit.setToolTip(QCoreApplication.translate("status_page", u"Quit", None))
+#endif // QT_CONFIG(tooltip)
+        self.pushButton_quit.setText(QCoreApplication.translate("status_page", u"Quit", None))
+#if QT_CONFIG(tooltip)
+        self.pushButton_close.setToolTip(QCoreApplication.translate("status_page", u"Close to tray/dock", None))
+#endif // QT_CONFIG(tooltip)
+        self.pushButton_close.setText(QCoreApplication.translate("status_page", u"Close", None))
+        self.label_onedrive_status.setText(QCoreApplication.translate("status_page", u"Onedrive is sync not running", None))
+        self.label_account_type.setText("")
+        self.pushButton_start_stop.setText(QCoreApplication.translate("status_page", u"start/stop", None))
+#if QT_CONFIG(tooltip)
+        self.pushButton_start.setToolTip(QCoreApplication.translate("status_page", u"Start Sync", None))
+#endif // QT_CONFIG(tooltip)
+        self.pushButton_start.setText(QCoreApplication.translate("status_page", u"Start", None))
+#if QT_CONFIG(tooltip)
+        self.pushButton_stop.setToolTip(QCoreApplication.translate("status_page", u"Stop Sync", None))
+#endif // QT_CONFIG(tooltip)
+        self.pushButton_stop.setText(QCoreApplication.translate("status_page", u"Stop", None))
+        self.label_version_check.setText("")
+#if QT_CONFIG(tooltip)
+        self.label_free_space.setToolTip(QCoreApplication.translate("status_page", u"Free Space", None))
+#endif // QT_CONFIG(tooltip)
+        self.label_free_space.setText("")
+#if QT_CONFIG(tooltip)
+        self.label_free_space_icon.setToolTip(QCoreApplication.translate("status_page", u"Free Space", None))
+#endif // QT_CONFIG(tooltip)
+        self.label_free_space_icon.setText(QCoreApplication.translate("status_page", u"TextLabel", None))
+#if QT_CONFIG(tooltip)
+        self.label_status.setToolTip(QCoreApplication.translate("status_page", u"Sync is stopped", None))
+#endif // QT_CONFIG(tooltip)
+        self.label_status.setText("")
+        self.pushButton_open_dir.setText(QCoreApplication.translate("status_page", u"Open Sync Directory", None))
+        self.pushButton_profiles.setText(QCoreApplication.translate("status_page", u"Profiles", None))
+        self.pushButton_gui_settings.setText(QCoreApplication.translate("status_page", u"GUI Settings", None))
+    # retranslateUi
+
